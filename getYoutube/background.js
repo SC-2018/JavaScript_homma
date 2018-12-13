12c3314e7daf5b247204ca107ac586ee974a088c
tacchan5424 <tacchan5424@yahoo.co.jp>
2018-12-13 22:30:20 +0900

    機能追加

--- getYoutube/background.js
+++ getYoutube/background.js
@@ -1,16 +1,55 @@
-var tag = document.createElement('script');
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+// popup.jsから各値の取得
+chrome.runtime.onMessage.addListener(
+  function (request) {
+    if (request.musicId)
+      bgSetMusic(request.musicId);
+    else if (request.volume)
+      bgSetVolume(request.volume);
+    else
+      bgSetTime(request.time);
+  }
+);
 
+// youtubeを埋め込む
+var music;
+function bgSetMusic(musicId) {
+  music = musicId;
+  var tag = document.createElement('script');
+  tag.src = "https://www.youtube.com/iframe_api";
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+}
+
+// playerの設定
 var player;
 function onYouTubeIframeAPIReady() {
-  player = new YT.Player('music', {
+  player = new YT.Player('musicPlayer', {
     height: '360',
     width: '640',
-    videoId: 'sYkgblk0Xt0',
+    videoId: music,
   });
 }
-function myPlayVideo() {
+
+// 音楽の再生
+function bgPlayMusic() {
   player.playVideo();
+  // 再生した動画の時間をpopup.jsに渡す
+  chrome.runtime.sendMessage({
+    "maxTime": player.getDuration()
+  });
+}
+
+// 音楽の一時停止
+function bgPauseMusic() {
+  player.pauseVideo();
+}
+
+// 音量の設定
+function bgSetVolume(volume) {
+  player.setVolume(volume);
+}
+
+// 再生時間の設定
+function bgSetTime(time) {
+  player.seekTo(time);
 }
--- getYoutube/popup.js
+++ getYoutube/popup.js
@@ -1,11 +1,92 @@
 // background.jsのオブジェクト登録
 var bgPage = chrome.extension.getBackgroundPage();
 
-// ckickイベントを生成
-var target = document.getElementById('playButton');
-target.addEventListener('click', bgPlay, false);
+// popup.htmlのイベントを生成
+// 再生イベント
+let target = document.getElementById('playButton');
+target.addEventListener('click', playMusic, false);
 
-// background.jsのイベントを呼び出す
-function bgPlay() {
-  bgPage.myPlayVideo();
+// 一時停止イベント
+target = document.getElementById('pauseButton');
+target.addEventListener('click', pauseMusic, false);
+
+// 音楽セットイベント
+target = document.getElementById('setButton');
+target.addEventListener('click', setMusic, false);
+
+// 拡張機能リロードイベント
+target = document.getElementById('reloadButton');
+target.addEventListener('click', reload, false);
+
+// 音楽登録イベント
+target = document.getElementById('recordButton');
+target.addEventListener('click', recordToLocalStorage, false);
+
+// 音量調整イベント
+target = document.getElementById('volumeBar');
+target.addEventListener('change', setVolume, false);
+
+// シークイベント
+target = document.getElementById('seekBar');
+target.addEventListener('change', setTime, false);
+
+// 音楽の選択
+if (localStorage.length != 0) {
+  for (let i = 0; i < localStorage.length; i++) {
+    let option = document.createElement("option");
+    option.innerText = localStorage.key(i);
+    option.value = localStorage.getItem(localStorage.key(i));
+    document.getElementById("selectMusic").appendChild(option);
+  }
 }
+
+// background.jsの再生イベントを呼び出す
+function playMusic() {
+  bgPage.bgPlayMusic();
+}
+
+// background.jsの停止イベントを呼び出す
+function pauseMusic() {
+  bgPage.bgPauseMusic();
+}
+
+// 再生する音楽の情報をbackground.jsに渡す
+function setMusic() {
+  chrome.runtime.sendMessage({
+    "musicId": document.getElementById("selectMusic").value
+  });
+}
+
+// 拡張機能のリロード
+function reload() {
+  chrome.runtime.reload();
+}
+
+// localStorageに音楽を登録
+function recordToLocalStorage() {
+  let key = document.getElementById("localStorageKey").value;
+  let value = document.getElementById("localStorageValue").value;
+  localStorage.setItem(key, value);
+}
+
+// background.jsの音量調整イベント呼び出す
+function setVolume() {
+  chrome.runtime.sendMessage({
+    "volume": document.getElementById("volumeBar").value
+  });
+}
+
+// 再生時間をbackground.jsに渡す
+function setTime() {
+  chrome.runtime.sendMessage({
+    "time": document.getElementById("seekBar").value
+  });
+}
+
+// 再生している動画の時間をbackground.jsから取得する
+chrome.runtime.onMessage.addListener(
+  function (request) {
+    if(request.maxTime)
+      document.getElementById("seekBar").max = request.maxTime;
+  }
+);
