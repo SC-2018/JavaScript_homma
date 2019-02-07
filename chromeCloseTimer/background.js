b1ef76b2bc0c8aa2865a627f054b59361ef2ad70
tacchan5424 <tacchan5424@yahoo.co.jp>
2019-02-07 22:54:58 +0900

    1桁時分でもタイマーセット可能となるように0詰め追加

--- chromeCloseTimer/background.js
+++ chromeCloseTimer/background.js
@@ -1,6 +1,5 @@
-// すべてのタブを閉じる
+// アクティブなwindowを閉じる
 function closeTabsAll() {
-    // 現在のwindowのすべてのタブを閉じる
     chrome.tabs.query({ 'currentWindow': true }, function (tabs) {
       for (let i = 0; i < tabs.length; i++)
         chrome.tabs.remove(tabs[i].id);
@@ -13,8 +12,15 @@ chrome.alarms.create('ChromeCloseTimer', { delayInMinutes: 1, periodInMinutes: 1
 // 1分ごとに時間を比較
 chrome.alarms.onAlarm.addListener(function () {
   let currentDateTime = new Date();
-  if (localStorage.getItem('closeTime') == (currentDateTime.getHours() + ":" + currentDateTime.getMinutes())) {
+  
+  // 時分を0詰めしてtype=timeとフォーマットを合わせる
+  if (localStorage.getItem('closeTime') == (zeroPadding(currentDateTime.getHours()) + ":" + zeroPadding(currentDateTime.getMinutes()))) {
     localStorage.removeItem("closeTime");
+    alert("時間になったのでクロームを強制終了します。");
     closeTabsAll();
   }
 });
+
+function zeroPadding(num) {
+  return ('00' + num).slice(-2);
+}
--- chromeCloseTimer/popup.js
+++ chromeCloseTimer/popup.js
@@ -5,36 +5,23 @@ target.addEventListener('click', recordToLocalStorage, false);
 target = document.getElementById('resetCloseTimeButton');
 target.addEventListener('click', reset, false);
 
-// 入力した時間をローカルストレージへ保存する
-function recordToLocalStorage(time) {
-  if (document.getElementById("closeTime").value != "" && localStorage.key(0) == null) {
+function recordToLocalStorage() {
+  // ローカルストレージに時間が登録されていない場合のみセットできる
+  if (document.getElementById("closeTime").value && !localStorage.getItem('closeTime')) {
     localStorage.setItem("closeTime", document.getElementById("closeTime").value);
-    document.getElementById("closeTime").value = null;
-  } else if (document.getElementById("closeTime").value != "" && localStorage.key(0) != null)
+    document.getElementById("closeTime").value = "";
+
+  // ローカルストレージに登録した時間の上書きはできず、リセットが必要
+  } else if (document.getElementById("closeTime").value && localStorage.getItem('closeTime'))
     alert(localStorage.getItem('closeTime') + "にセットしてあります。")
+
   else
     alert("時間をセットしてください。");
 }
 
-// ローカルストレージに保存した時間を削除する
 function reset() {
-  if (localStorage.key(0))
+  if (localStorage.getItem('closeTime'))
     localStorage.removeItem("closeTime");
 }
 
-// localStorageの有無を判定し、ボタン表示を切り替える
-function checkHasLocalStorage() {
-  if (localStorage.key(0)) {
-    document.getElementById("setCloseTimeButton").style.display = none;
-    document.getElementById("resetCloseTimeButton").style.display = block;
-  }
-}
-
-// window.onload = checkHasLocalStorage();
-
-// ToDo
-// storageが存在しない場合の処理追加
-// onloadが動くようにする
-// resetの名前
-// 時分の0の処理
-// localStorage.key(0)以外の指定方法
+// 強制終了前にブラウザを閉じた場合の処理追加
